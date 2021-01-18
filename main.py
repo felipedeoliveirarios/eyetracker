0c15cc8141605575d03da6f0d4d0047579b400d1
felipedeoliveirarios <felipedeoliveirarios@gmail.com>
2021-01-18 05:18:35 -0300

    Iteração no projeto.

--- main.py
+++ main.py
@@ -12,17 +12,28 @@ ao formato aparente da íris. Em seguida, a elipse é assumida como um círculo
 inclinado, e calculando-se essa inclinação, obtém-se uma boa aproximação do
 ângulo de rotação dos olhos. Extendendo-se as normais dos planos relativos aos
 círculos, obtém-se, no ponto de interseção das normais, uma estimativa do foco
-visual. 
+visual.
+
+O algoritmo de ellipse-fitting utilizado é o de mínimos quadrados, implementado
+por Ben Hammel e Nick Sullivan-Molina, em 21 de março de 2020, à partir da 
+publicação "Numerically Stable Direct Least Squares Fitting Of Ellipses", por
+Radim Halír e Jan Flusser.
+
+Através deste algoritmo, tentou-se provar que a aplicação de novas técncas na
+segmentação das íris seria capaz de causar uma melhoria no desempenho do método
+proposto por Wang et. al., mas problemas no tratamento de imagens tornam os
+resultados inconclusivos.
 
 Por Felipe Rios
 '''
 
-import cv2
 import dlib
+import cv2
 import numpy as np
 import argparse
 import os.path
 import pyautogui
+from ellipse import LsqEllipse
 
 right_eye_landmarks = [36, 37, 38, 39, 40, 41]
 left_eye_landmarks = [42, 43, 44, 45, 46, 47]
@@ -83,8 +94,8 @@ def filter_eye_image(image, mask=None):
 	# Define as matrizes a serem utilizadas nas operações morfológicas.
 	kernel = cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (3, 3))
 	kernel2 = cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (15, 15))
-	kernel3 = cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (7, 7))
-	vertical_operator = np.array([[-1, 0, 1], [-1, 0, 1], [-1, 0, 1]])
+	kernel3 = cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (5, 5))
+	vertical_operator = np.array([[0, 0, 0], [1, -2, 1], [0, 0, 0]])
 
 	# Converte a imagem p/ escala de cinza e equaliza o histograma.
 	image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
@@ -95,11 +106,10 @@ def filter_eye_image(image, mask=None):
 
 	# Aplica operações morfológicas para reduzir a quantidade de informação
 	image = cv2.morphologyEx(image, cv2.MORPH_OPEN, kernel)
-	image = cv2.morphologyEx(image, cv2.MORPH_CLOSE, kernel3)
-	image = cv2.morphologyEx(image, cv2.MORPH_OPEN, kernel)
+	image = cv2.morphologyEx(image, cv2.MORPH_CLOSE, kernel)
+	image = cv2.morphologyEx(image, cv2.MORPH_OPEN, kernel3)
 	image = cv2.bitwise_not(image)
 	image = cv2.bitwise_and(image, image, mask=cv2.erode(mask, kernel2))
-	image = cv2.morphologyEx(image, cv2.MORPH_OPEN, kernel)
 
 	# Encontra os contornos de todas as áreas brancas.
 	contours, _ = cv2.findContours(image, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE)
@@ -111,38 +121,34 @@ def filter_eye_image(image, mask=None):
 	# Cria uma máscara que mantém apenas o maior contorno
 	mask_hole = contours[max_index]
 	mask = np.zeros((w, h), np.uint8)
-	mask = cv2.fillConvexPoly(mask, mask_hole, 255)
+	mask = cv2.fillPoly(mask, mask_hole, 255)
 
-	# Aplica a máscara e mantém apenas a maior área branca na imagem (a íris)
+	# Aplica a máscara e mantém apenas a maior área branca da imagem invertida (a íris)
 	image = cv2.bitwise_and(image, image, mask=mask)
 
-	'''
-	# Separa as duas bordas laterais da íris.
-	image_left = cv2.filter2D(image, -1, -vertical_operator)
-	image_right = cv2.filter2D(image, -1, vertical_operator)
-	image = image_left + image_right
-	'''
+	# Aplica o operador de borda vertical.
+	image = cv2.filter2D(image, -1, vertical_operator)
 
 	return image
 
-def find_ellipses(contours):
+def ellipse_fit(image):
 	'''
-	Dado um conjunto de contornos, encontra elipses que se aproximam do formato.
+	Dada uma imagem binária, extrai os pixels brancos como vértices e busca uma
+	elipse que se encaixe neles.
 
 	Parâmetros:
-		contours (np.array<np.array>): os contornos a serem analisados.
+		image (np.array): array contendo os pixels da imagem.
 
 	Retorno:
-		lista<cv.rotatedRect>: Lista contendo os dados das elipses.
+		ellipse_data (ellipse.LsqEllipse): elipse detectada.
 	'''
-	ellipses = []
-	if len(contours) != 0:
-		for cont in contours:
-			if len(cont) < 5:
-				break
-			elps = cv2.fitEllipse(cont)
-			ellipses.append(elps)
-	return ellipses
+
+	white_pixels = np.argwhere(image == 255)
+	ellipse_data = LsqEllipse().fit(white_pixels)
+	center, width, height, angle = ellipse_data.as_parameters()
+	center = (center[1], center[0])
+	ellipse_data = (center, (width*2, height*2), angle)
+	return ellipse_data;
 
 def auto_resize(image):
 	'''
@@ -179,23 +185,50 @@ def auto_resize(image):
 
 	return image
 
-def run(imagePath, show=False, output=None):
+def get_normal(tilt, slant):
+	'''
+	Obtém a normal de um plano, considerando a modelagem feita.
+
+	Parâmetros:
+		tilt (float): ângulo em graus do tilt (vide modelagem)
+		slant (float): ângulo em graus do slant (vide modelagem)
+
+	Retorno:
+		np.array: Vetor normal do plano.
+	'''
+	normal = np.array([0, 0, 1]) # Normal do plano, com tilt e slant zero.
+
+	# Pré-calcula senos e cossenos.
+	tilt_cos = np.cos(np.deg2rad(tilt))
+	tilt_sin = np.sin(np.deg2rad(tilt))
+
+	slant_cos = np.cos(np.deg2rad(slant))
+	slant_sin = np.sin(np.deg2rad(slant))
+
+	# Matrizes de rotação sobre os ângulos nos eixos y e z.
+	rotation_tilt = np.array([[tilt_cos, -tilt_sin, 0],
+							 [tilt_sin, tilt_cos, 0],
+							 [0, 0, 1]])
+
+	rotation_slant = np.array([[slant_cos, 0, slant_sin],
+							  [0, 1, 0],
+							  [-slant_sin, 0, slant_cos]])
+
+	# Efetua a multiplicação de matrizes.
+	normal = np.dot(normal, rotation_tilt)
+	normal = np.dot(normal, rotation_slant)
+	return normal
+
+def run(img, show=False, output=None):
 	'''
 	Segmenta as íris da imagem e tenta aplicar o método proposto por J.-G. Wang
 	et. al. para determinar o ângulo de rotação dos olhos.
 
 	Parâmetros:
-		imagePath (string): caminho válido para uma imagem
-		show (bool): flag de exibição dos resultados
-		output (string): local de salvamento da saída
+		img (np.array): array do numpy contendo a imagem.
+		show (bool): flag de exibição dos resultados. Opcional.
+		output (string): local de salvamento da saída. Opcional.
 	'''
-	# Carrega a imagem
-	img = cv2.imread(imagePath)
-
-	# Testa se foi possível carregar a imagem
-	if img.size == 0:
-		print('Image file not found.')
-		return
 
 	# Cria cópias da imagem para visualização.
 	original = img.copy()
@@ -273,38 +306,21 @@ def run(imagePath, show=False, output=None):
 		left_eye_img = filter_eye_image(left_eye_img, left_eye_mask)
 		right_eye_img = filter_eye_image(right_eye_img, right_eye_mask)
 
-		# Procura os contornos das íris nas imagens dos olhos.
-		left_eye_contours, _ = cv2.findContours(left_eye_img, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
-		right_eye_contours, _ = cv2.findContours(right_eye_img, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
-
-		# Tenta encontrar elipses entre os contornos dos olhos.
-		'''
-		Este método é diferente do proposto por J.-G. Wang el. al., e essa é a
-		provável causa da falta de acurácia da aplicação.
-		'''
-		left_eye_ellipses = find_ellipses(left_eye_contours)
-		right_eye_ellipses = find_ellipses(right_eye_contours)
+		# Aplica o ellipse-fitting em ambas as imagens contendo as bordas laterais das íris.
+		left_eye_ellipse = ellipse_fit(left_eye_img)
+		right_eye_ellipse = ellipse_fit(right_eye_img)
 
 		# Troca o esquema de cores das imagens dos olhos de volta para colorida.
 		left_eye_img = cv2.cvtColor(left_eye_img, cv2.COLOR_GRAY2BGR)
 		right_eye_img = cv2.cvtColor(right_eye_img, cv2.COLOR_GRAY2BGR)
 
-		# Desenha os contornos nas imagens dos olhos.
-		cv2.drawContours(left_eye_img, left_eye_contours, -1, (0, 0, 255), 1)
-		cv2.drawContours(right_eye_img, right_eye_contours, -1, (0, 0, 255), 1)
 
 		# Desenha as elipses nas imagens dos olhos e do rosto.
-		for ellipse in left_eye_ellipses:
-			cv2.ellipse(left_eye_img, ellipse, (0, 255, 0), 1)
-			cv2.ellipse(original[lb_start[1]:lb_end[1], lb_start[0]:lb_end[0]], ellipse, (0, 255, 0), 1)
+		cv2.ellipse(left_eye_img, left_eye_ellipse, (0, 255, 0), 1)
+		cv2.ellipse(original[lb_start[1]:lb_end[1], lb_start[0]:lb_end[0]], left_eye_ellipse, (0, 255, 0), 1)
 
-		for ellipse in right_eye_ellipses:
-			cv2.ellipse(right_eye_img, ellipse, (0, 255, 0), 1)
-			cv2.ellipse(original[rb_start[1]:rb_end[1], rb_start[0]:rb_end[0]], ellipse, (0, 255, 0), 1)
-
-		# Separa as elipses das íris
-		left_eye_ellipse = left_eye_ellipses[0]
-		right_eye_ellipse = right_eye_ellipses[0]
+		cv2.ellipse(right_eye_img, right_eye_ellipse, (0, 255, 0), 1)
+		cv2.ellipse(original[rb_start[1]:rb_end[1], rb_start[0]:rb_end[0]], right_eye_ellipse, (0, 255, 0), 1)
 
 		'''
 		A rotação do olho é modelada em apenas dois ângulos: tilt, sobre o eixo
@@ -350,43 +366,116 @@ def run(imagePath, show=False, output=None):
 
 		# O else definiria o slant como 0, mas já é o valor padrão.
 
+		# Calcula as normais para os dois círculos, projetados como elipses.
+		left_eye_normal = get_normal(left_eye_tilt, left_eye_slant)
+		right_eye_normal = get_normal(right_eye_tilt, right_eye_slant)
+
 		# Exibe os dados obtidos para o rosto atual.
+		
 		print('\nFace {}:'.format(i))
-		print('\tLeft Eye:\ttilt: [{tilt: .2f}°;\tslant: {slant: .2f}°]'.format(
-			tilt=left_eye_tilt, slant=left_eye_slant))
-		print('\tRight Eye:\ttilt: [{tilt: .2f}°;\tslant: {slant: .2f}°]'.format(
-			tilt=right_eye_tilt, slant=right_eye_slant))
+		print('\tLeft Eye:\n\t\ttilt: [{tilt: .2f}°;\tslant: {slant: .2f}°;\n\t\tnormal: {normal}]'.format(
+			tilt=left_eye_tilt, slant=left_eye_slant, normal=left_eye_normal))
+		print('\tRight Eye:\n\t\ttilt: [{tilt: .2f}°;\tslant: {slant: .2f}°;\n\t\tnormal: {normal}]'.format(
+			tilt=right_eye_tilt, slant=right_eye_slant, normal=right_eye_normal))
+		print('-'*80)
 
-		# Exibe os resultados parciais e o resultado final.
+		return_list = []
+
+		# Adiciona os resultados parciais e o resultado final na lista de retorno.
 		if show:
-			preview = auto_resize(preview)
-			cv2.imshow("preview", preview)
-
-			cv2.imshow("Left Eye", left_eye_img)
-
-			cv2.imshow("Right Eye", right_eye_img)
-
-			original = auto_resize(original)
-			cv2.imshow("Result", original)
-			cv2.waitKey()
-			cv2.destroyAllWindows()
+			return_list.append(auto_resize(preview))
+			return_list.append(left_eye_img)
+			return_list.append(right_eye_img)
+			return_list.append(auto_resize(original))
 
 		if output:
 			cv2.imwrite(output, original)
 
+		return return_list
+
+def run_image(imagepath, show=False, output=None):
+	# Carrega a imagem
+	img = cv2.imread(imagepath)
+
+	# Testa se foi possível carregar a imagem
+	if img.size == 0:
+		print('Image file not found.')
+		return
+
+	results = run(img, show, output)
+
+	for i, image in enumerate(results):
+		cv2.imshow(str(i), image)
+
+	cv2.waitKey(0)
+	cv2.destroyAllWindows()
+
+def run_video(videopath, show=False, output=None):
+	capture = cv2.VideoCapture(videopath)
+
+	if not capture:
+		print('Video file not found.')
+		return
+
+	while(capture.isOpened()):
+		ret, frame = capture.read()
+		results = run(frame, show)
+
+		if not results:
+			continue
+		for i, image in enumerate(results):
+			cv2.imshow(str(i), image)
+		if cv2.waitKey(1) & 0xFF == ord('q'):
+			break
+
+	capture.release()
+	cv2.destroyAllWindows()
+
+def run_camera(show=False):
+	capture = cv2.VideoCapture(0)
+	while(True):
+		ret, frame = capture.read()
+		results = run(frame, show)
+
+		if not results:
+			continue
+		for i, image in enumerate(results):
+			cv2.imshow(str(i), image)
+		if cv2.waitKey(1) & 0xFF == ord('q'):
+			break
+
+	capture.release()
+	cv2.destroyAllWindows()
+
 def main():
 	description = 'Searches for irises and tries to find its rotation angles.'
 	parser = argparse.ArgumentParser(description = description)
-	parser.add_argument('image', help='The path to an image to be tested.')
+	parser.add_argument('-i', '--image', help='The path to an image to be tested.', action='store')
+	parser.add_argument('-v', '--video', help='The path to a video to be tested.', action='store')
+	parser.add_argument('-c', '--camera', help='Uses the camera input instead of an image.', action='store_true')
 	parser.add_argument('-o', '--output', help='Path to an output file.', default=None)
 	parser.add_argument('--show', help='Flag to show the images at the end.', action='store_true')
 	args = vars(parser.parse_args())
 
-	if not os.path.exists(args['image']):
-		parser.error("The file %s does not exist!" % args['image'])
-		return
+	if args['image']:
+		if not os.path.exists(args['image']):
+			parser.error("The file \"{}\" does not exist!".format(args['image']))
+		else:
+			run_image(args['image'], args['show'], args['output'])
 
-	run(args['image'], args['show'], args['output'])
+	elif args['video']:
+		if not os.path.exists(args['video']):
+			parser.error("The file \"{}\" does not exist!".format(args['video']))
+		else:
+			run_video(args['video'], args['show'], args['output'])
+
+
+	elif args['camera']:
+		run_camera(args['show'])
+
+	else:
+		parser.error("Please select a source using the params -i, -v or -c.")
+		
 
 if __name__ == "__main__":
 	main()
